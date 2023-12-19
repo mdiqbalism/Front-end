5991fd9421ac45bd313c3c4b56a6b95fc8a7a4fc
Md Iqbal Ali <mdiqbalali78692@gmail.com>
2023-12-20 00:59:29 +0530

    initial commit

--- JAVASCRIPT/04_control_flow/01_one.js
+++ JAVASCRIPT/04_control_flow/01_one.js
@@ -13,7 +13,39 @@
 // Nullish Coalesing Operator (??): null undefined 
 
 let val1;
-// val1 = 5 ??10
+ val1 = 5 ??10
 val1 = null??10
 
 console.log(val1);
+
+// switch
+// SYNTAX
+
+// swith (key){
+//     case Value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 3:
+//         console.log("April");
+//         break;
+    
+//     default:
+//         console.log("Not match");
+//         break;
+// }
